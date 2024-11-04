97fc586ac6978f5ef38a11c9f8ac648b4ea36b4e
cyberman443 <badakhsh44@gmail.com>
2024-11-04 11:42:40 +0430

    Day 1 Course practicing is underway now.

---
+++ Day 1 - Course Practices/input.py
@@ -0,0 +1,7 @@
+# input("What is your name, sir/madam? ")
+
+
+print("Hello ", input("What is your name, sir/madam? "), "!")
+
+
+print("Hello " + input("What is your name, sir/madam? ") + "!")
---
+++ Day 1 - Course Practices/print.py
@@ -0,0 +1,68 @@
+print("This is a printing practice file")
+
+
+print("""
+      
+      This repository is a manifestation for 100 days of practicing python 
+      along with keeping up with "Udemy - 100 Days of Code The Complete Python Pro Bootcamp" course, 
+      to become a Professional Python Developer by coding advanced apps and tools in python.
+      
+      """)
+
+
+def app_init_description_print_function():
+    description = """
+    
+    🌟 Brand Name Generator 🌟
+
+    Unlock your brand's potential with our AI-powered generator!
+    
+    ✨ Features:
+    - **Keywords**: Infuse your brand with meaningful words.
+    - **City Name**: Add a local flair to connect with your community.
+    - **Business Type**: Tailor names to your industry for better resonance.
+    - **Themes**: Convey emotions like innovation, elegance, or sustainability.
+    - **Target Audience**: Align names with your ideal customers.
+
+    Combine options or use them separately to create a unique brand identity. Let your imagination soar!
+    
+    """
+    
+    print(description)
+    
+    
+app_init_description_print_function()
+
+
+
+def app_init_description_print_function_one_line():
+    description = "\t🌟 Brand Name Generator 🌟"+"\n\n\tUnlock your brand's potential with our AI-powered generator!"+"\n\n\t✨ Features:"+"\n\n\t- **Keywords**: Infuse your brand with meaningful words.\n\t- **City Name**: Add a local flair to connect with your community.\n\t- **Business Type**: Tailor names to your industry for better resonance.\n\t- **Themes**: Convey emotions like innovation, elegance, or sustainability.\n\t- **Target Audience**: Align names with your ideal customers. \n\t Combine options or use them separately to create a unique brand identity. Let your imagination soar!"
+    
+    print(description)
+    
+    
+app_init_description_print_function_one_line()
+
+
+def app_init_description_print_function_two_line():
+    description = """
+    
+    🌟 Brand Name Generator 🌟
+
+    Unlock your brand's potential with our AI-powered generator!
+    
+    ✨ Features:
+    - **Keywords**: Infuse your brand with meaningful words.
+    - **City Name**: Add a local flair to connect with your community.
+    - **Business Type**: Tailor names to your industry for better resonance.
+    - **Themes**: Convey emotions like innovation, elegance, or sustainability.
+    - **Target Audience**: Align names with your ideal customers.
+
+    Combine options or use them separately to create a unique brand identity. Let your imagination soar!
+    
+    """
+    
+    print(description)
+    
+    
+app_init_description_print_function_two_line()
